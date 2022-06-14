523b06bad505fc096db630e34f478fbc92a2582b
Joshua L. Adelman <joshua.adelman@gmail.com>
2022-06-14 13:44:11 -0400

    try to deal with failures

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,6 +7,9 @@ export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER=$CC
 export CARGO_TARGET_X86_64_APPLE_DARWIN_LINKER=$CC
 export CARGO_TARGET_AARCH64_APPLE_DARWIN_LINKER=$CC
 
+# https://github.com/rust-lang/cargo/issues/10583#issuecomment-1129997984
+export CARGO_NET_GIT_FETCH_WITH_CLI=true
+
 # Print Rust version
 rustc --version
 
